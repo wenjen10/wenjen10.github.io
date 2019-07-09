253875e82c3c190e69630b9880d156bf8f772b2e
wenjen10 <wenjen10@yahoo.com>
2019-07-09 16:23:05 -0400

    portfolio changes to multiple files

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 var btn = document.createElement("BUTTON");
 
 
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello, my name is Jen Barbosa. I’m from Oberlin, Ohio. Currently learning at WCCIT! ";
+}
+</script>
+
+
+// Dynamically change the color of the background on the footer when the user hovers over it
+const footer = document.querySelector("footer");
+footer.addEventListener("mouseover", function() {
+footer.style.backgroundColor = "grey";
+});
+
+
